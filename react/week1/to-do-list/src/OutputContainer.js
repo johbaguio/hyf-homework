dcedd2261ddda27589d187b136da85b2fdebb86b
Johanna <johbaguio@gmail.com>
2019-02-19 22:27:20 +0100

    Updated files

--- react/week1/to-do-list/src/OutputContainer.js
+++ react/week1/to-do-list/src/OutputContainer.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import ToDoList from './List';
 import Footer from './Footer';
 
-const listDescriptions = ['򪪪 Get out of bed', '򪪪 Brush teeth', '򪪪 Eat breakfast'];
+const listDescriptions = ['򪪪  Get out of bed', '򪪪  Brush teeth', '򪪪  Eat breakfast'];
 const dueDates = ['Wed Sep 13 2017', 'Thu Sep 14 2017', 'Fri Sep 15 2017'];
 
 class Output extends React.Component{
