bd6c8118c0c438b881f5094178bc9a23522bcc64
Johanna <johbaguio@gmail.com>
2019-03-14 00:44:42 +0100

    Added files for search app using github's api.

---
+++ react/week4/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Container from './Container'
+
+
+
+
+
+ReactDOM.render(<Container />, document.getElementById('root'));
+
+
